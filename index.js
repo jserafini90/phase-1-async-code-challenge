6c21e124021938d113e92a8212a99afcca2b9309
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-08-17 20:57:30 -0400

    Created the list of movie titles and had them appear under show. Also started working with the addEventListener for the search bar

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+const searchInput = document.querySelector('[data-search]')
+
+  let search = []
+
+ searchInput.addEventListener("input", (e) => {
+     const value = e.target.value.toLowerCase()
+     search.forEach(search => {
+        const isVisible = 
+        search.title.toLowerCase().includes(value)
+        search.element.classList.toggle("hide", !isVisible)
+     })
+      
+})
+ 
+
 fetch('http://localhost:3000/shows')
 .then(res => res.json())
 .then(data => {
-data.forEach(name => {
-       console.log(name.title)
+    const list = document.getElementById('list')
+    search = data.map(movie => {
+        let title = document.createElement('li')
+        title.innerText = movie.title
+        //console.log(movie)
+        list.append(title)
+        return {title: movie.title}
+        //list.addEventListener('click', (e) => {
    }) 
 })
+//})
+    
